641501b5f3c013d98269065ffc43c90c1248a40a
k-narusawa <swkei320@gmail.com>
2023-06-03 13:38:32 +0900

    auth0やめる

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,18 +1,30 @@
 import NextAuth from "next-auth";
-import Auth0Provider from "next-auth/providers/auth0";
 
 export default NextAuth({
   providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID!,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
-      issuer: process.env.AUTH0_ISSUER_BASE_URL,
+    {
+      id: "keycloak",
+      name: "KeyCloak",
+      type: "oauth",
+      wellKnown: process.env.KEY_CLOAK_BASE_URL + "/auth/realms/sample/.well-known/openid-configuration",
+      clientId: process.env.KEY_CLOAK_CLIENT_ID,
+      clientSecret: process.env.KEY_CLOAK_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'openid offline',
-        },
-      }
-    })
+          scope: "openid" 
+        }
+      },
+      idToken: true,
+      checks: [
+        "pkce", 
+        "state"
+      ],
+      profile(profile) {
+        return {
+          id: profile.sub,
+        }
+      },
+    },
   ],
   callbacks: {
     async jwt({ token, account }) {
@@ -25,6 +37,10 @@ export default NextAuth({
     async session({ session, token }) {
       session.user.accessToken = token.access_token
       return session;
+    },
+
+    async redirect({ url, baseUrl }) {
+      return baseUrl
     },
   },
 })
